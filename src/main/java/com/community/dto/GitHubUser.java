7f1ab5b3906972aa06fa71849496d2954d643b51
hongwei <479911822@qq.com>
2019-11-20 15:25:56 +0800

    extract github config from code to properties

---
+++ src/main/java/com/community/dto/GitHubUser.java
@@ -0,0 +1,23 @@
+package com.community.dto;
+
+
+public class GitHubUser {
+    private String login;
+    private  Long id;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
