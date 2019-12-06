cdc27b52ed9dfee572919faab495123728fecb9c
hongwei <479911822@qq.com>
2019-12-06 17:33:25 +0800

    news

--- src/main/java/com/community/advice/CustomizeExceptionHandler.java
+++ src/main/java/com/community/advice/CustomizeExceptionHandler.java
@@ -1,23 +1,40 @@
 package com.community.advice;
 
+import com.community.dto.ResultDTO;
+import com.community.exception.CustomizeErrorCode;
 import com.community.exception.CustomizeException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @ControllerAdvice
 public class CustomizeExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    ModelAndView handle(Throwable e, Model model){
-        if(e instanceof CustomizeException){
-            model.addAttribute("message",e.getMessage());
-        }else{
-            model.addAttribute("message","服务冒烟了，请稍后再试！！！");
+    Object handle(Throwable e, Model model, HttpServletRequest request){
+
+        String contentType = request.getContentType();
+        if("application/json".equals(contentType)){
+            //返回JSON
+            if(e instanceof CustomizeException){
+                return ResultDTO.errorOf((CustomizeException) e);
+            }else{
+                return ResultDTO.errorOf(CustomizeErrorCode.SYS_ERROR);
+            }
+        }else {
+            //错误页面跳转
+
+            if(e instanceof CustomizeException){
+                model.addAttribute("message",e.getMessage());
+            }else{
+                model.addAttribute("message","服务冒烟了，请稍后再试！！！");
+            }
+            return new ModelAndView("error");
         }
-        return new ModelAndView("error");
     }
 
 }
--- src/main/java/com/community/controller/CommentController.java
+++ src/main/java/com/community/controller/CommentController.java
@@ -1,21 +1,37 @@
 package com.community.controller;
 
 import com.community.dto.CommentDTO;
+import com.community.dto.ResultDTO;
+import com.community.exception.CustomizeErrorCode;
 import com.community.mapper.CommentMapper;
 import com.community.model.Comment;
+import com.community.model.User;
+import com.community.service.CommentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class CommentController {
 
     @Autowired
-    private CommentMapper commentMapper;
+    private CommentService commentService;
 
+    @ResponseBody
     @PostMapping("/comment")
-    public Object post(@RequestBody CommentDTO commentDTO){
+    public Object post(@RequestBody CommentDTO commentDTO,
+                       HttpServletRequest request){
+        User user = (User) request.getSession().getAttribute("user");
+        if(user == null){
+            return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);  //如果没有登录则抛异常
+        }
         Comment comment = new Comment();
         comment.setParentId(commentDTO.getParentId());
         comment.setContent(commentDTO.getContent());
@@ -24,7 +40,7 @@ public class CommentController {
         comment.setGmtModified(System.currentTimeMillis());
         comment.setCommentator(1);
         comment.setLikeCount(0L);
-        commentMapper.insert();
-        return null;
+        commentService.insert(comment);
+        return ResultDTO.okOf();
     }
 }
